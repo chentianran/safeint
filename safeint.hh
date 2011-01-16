11e91141d4a2983e5b68bb5b8fc32a1d475fb907
Nicholas Ovenhouse <ovenhou3@msu.edu>
2011-01-16 14:57:22 -0800

    added detection of negative overflow to SafeIntLong operator* and operator+ defined remaining operator overloads for SafeIntFloat and SafeIntLim

--- safeint.hh
+++ safeint.hh
@@ -1,3 +1,8 @@
+#include <limits.h>
+#include <math.h>
+
+
+
 //------------------------------------------------------------------
 // SafeIntLong class
 //
@@ -67,9 +72,20 @@ inline SafeIntLong operator * (SafeIntLong& lhs, SafeIntLong& rhs)
     long long longLeft = (long long) lhs._val;
     long long longRight = (long long) rhs._val;
 
+    // bit patterns
+    unsigned leftBits = (unsigned) lhs._val;
+    unsigned rightBits = (unsigned) rhs._val;
+
     long long product = longLeft * longRight;
-    if(product > (long long) INT_MAX)
-        throw "overflow";
+    
+    // If the signs are the same, test product against max,
+    //   otherwise, test against min integer value
+    if( ~(leftBits ^ rightBits) & 0x80000000u )
+        if(product > (long long) INT_MAX)
+            throw "overflow";
+    else
+        if(product < (long long) INT_MIN)
+            throw "overflow";
 
     return SafeIntLong(product);
 }
@@ -128,7 +144,7 @@ SafeIntFloat operator % (SafeIntFloat&, SafeIntFloat&);
 bool operator== (SafeIntFloat&, SafeIntFloat&);
 
 
-SafeIntFloat& SafeIntFloat::operator=(SafeIntFloat& arg)
+inline SafeIntFloat& SafeIntFloat::operator=(SafeIntFloat& arg)
 {
     _val = arg._val;
 
@@ -136,26 +152,186 @@ SafeIntFloat& SafeIntFloat::operator=(SafeIntFloat& arg)
 }
 
 
-// SafeIntFloat operator + (SafeIntFloat
+inline SafeIntFloat operator + (SafeIntFloat& lhs, SafeIntFloat& rhs)
+{
+    // Bit patterns
+    unsigned leftBits = (unsigned) lhs._val;
+    unsigned rightBits = (unsigned) rhs._val;
+
+    float leftFloat = (float) lhs._val;
+    float rightFloat = (float) rhs._val;
+
+    
+    // If the signs are the same, just test the sum of the absolute values against the max
+    // If the signs are different, there can be no overflow
+    float sum = fabs(leftFloat) + fabs(rightFloat);
+    if( ~(leftBits ^ rightBits) & 0x80000000u  && sum > (float) INT_MAX)
+        throw "overflow";
+        return SafeIntFloat(0);
+
+
+    return SafeInt( lhs._val + rhs._val );
+}
+
+
+inline SafeIntFloat operator - (SafeIntFloat& lhs, SafeIntFloat& rhs)
+{
+    SafeIntFloat negRhs( -(rhs._val) );
+
+    return lhs + negRhs;
+}
+
+
+inline SafeIntFloat operator * (SafeIntFloat& lhs, SafeIntFloat& rhs)
+{
+    float leftFloat = (float) lhs._val;
+    float rightFloat = (float) rhs._val;
+
+    float product = leftFloat * rightFloat;
+    if(product > (float) INT_MAX || product < (float) INT_MIN)
+        throw "overflow";
+
+
+    return SafeIntFloat( lhs._val * rhs._val );
+}
+
+
+inline SafeIntFloat operator / (SafeIntFloat& lhs, SafeIntFloat& rhs)
+{
+    return SafeIntFloat( lhs._val / rhs._val );
+}
+
+
+inline SafeIntFloat operator % (SafeIntFloat& lhs, SafeIntFloat& rhs)
+{
+    return SafeIntFloat( lhs._val % rhs._val );
+}
+
+
+inline bool operator == (SafeIntFloat& lhs, SafeIntFloat& rhs)
+{
+    return lhs._val == rhs._val;
+}
 //
 // End SafeIntFloat operators
 //--------------------------------------------------------------------------    
 
+
+
+//--------------------------------------------------------------------------
+// SafeIntLim class
+//
 class SafeIntLim
 {
-private:
-
-    int _val;
-
 public:
+    int _val;
 
     SafeIntLim (int value) : _val (value) { };
     SafeIntLim& operator = (SafeIntLim&);
 };
+//
+//----------------------------------------------------------------------------
 
+
+//----------------------------------------------------------------------------
+// SafeIntLim operators
+//
 SafeIntLim operator + (SafeIntLim&, SafeIntLim&);
 SafeIntLim operator - (SafeIntLim&, SafeIntLim&);
 SafeIntLim operator * (SafeIntLim&, SafeIntLim&);
 SafeIntLim operator / (SafeIntLim&, SafeIntLim&);
 SafeIntLim operator % (SafeIntLim&, SafeIntLim&);
-SafeIntLim operator== (SafeIntLim&, SafeIntLim&);    
+SafeIntLim operator== (SafeIntLim&, SafeIntLim&);
+
+
+inline SafeIntLim& SafeIntLim::operator=(SafeIntLim& arg)
+{
+    _val = arg._val;
+    
+    return *this;
+}
+
+
+inline SafeIntLim operator + (SafeIntLim& lhs, SafeIntLim& rhs)
+{
+    unsigned leftBits = (unsigned) lhs._val;
+    unsigned rightBits = (unsigned) rhs._val;
+
+    // Only same-sign additions can overflow
+    if( ~(leftBits ^ rightBits) & 0x80000000u )
+    {
+        // Test negative sum against min value
+        if(leftBits & 0x80000000u && lhs._val < INT_MIN - rhs._val)
+            throw "overflow";
+
+        // Test positive sum against max value
+        else if( !(leftBits & 0x80000000u) && lhs._val > INT_MAX - rhs._val)
+            throw "overflow";
+    }
+
+    return SafeIntLim( lhs._val + rhs._val );
+}
+
+
+inline SafeIntLim operator - (SafeIntLim& lhs, SafeIntLim& rhs)
+{
+    SafeIntLim negRhs(-rhs._val);
+
+    return lhs + negRhs;
+}
+
+
+inline SafeIntLim operator * (SafeIntLim& lhs, SafeIntLim& rhs)
+{
+    // Bit patterns
+    unsigned leftBits = (unsigned) lhs._val;
+    unsigned rightBits = (unsigned) rhs._val;
+
+    // Operands have same sign...
+    //    overflow will happen in the positive direction
+    if( ~(leftBits ^ rightBits) & 0x80000000u )
+    {
+        // positive
+        if( !(leftBits & 0x80000000u) && lhs._val > INT_MAX / rhs._val )
+            throw "overflow";
+
+        // negative
+        else if(leftBits & 0x80000000u && lhs._val < INT_MAX / rhs._val)
+            throw "overflow";
+
+    }
+
+    // Operands have opposite sign...
+    //    overflow will happen in the negative direction
+    else
+    {
+        // second operand is positive
+        if( !(leftBits & 0x80000000u) && lhs._val < INT_MIN / rhs._val )
+            throw "overflow";
+
+        // second operand is negative
+        if(leftBits & 0x80000000u && lhs._val > INT_MIN / rhs._val)
+            throw "overflow";
+    }
+
+
+    return SafeIntLim( lhs._val * rhs._val );
+}
+
+
+inline SafeIntLim operator / (SafeInt& lhs, SafeIntLim& rhs)
+{
+    return SafeIntLim( lhs._val / rhs._val );
+}
+
+
+inline SafeIntLim operator % (SafeIntLim& lhs, SafeIntLim& rhs)
+{
+    return SafeIntLim( lhs._val % rhs._val );
+}
+
+
+inline bool operator == (SafeIntLim& lhs, SafeIntLim& rhs)
+{
+    return lhs._val == rhs._val;
+}
